05ac3e1089569c5f51cf97c6c32304720837cb4d
Sefdine <74672023+Sefdine@users.noreply.github.com>
2023-12-11 09:32:09 +0100

    extract users

---
+++ app/extract/users.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+def getUsers():
+    # Create a Spark session
+    spark = SparkSession.builder.appName("getUsers").getOrCreate()
+
+    # Define the schema based on your column names and data types
+    schema = StructType([
+        StructField("index", IntegerType(), True),
+        StructField("user_id", IntegerType(), True),
+        StructField("gender", StringType(), True),
+        StructField("occupation", StringType(), True),
+        StructField("zip", StringType(), True)
+    ])
+
+    # Specify the file path
+    user_path = "../data/ml-100k/u.user"
+
+    # Read the CSV file with the specified schema
+    user_df = spark.read.csv(user_path, sep='|', header=False, schema=schema)
+
+    # Select only the columns you need
+    user_df = user_df.select("user_id", "gender", "occupation", "zip")
+
+    # Return the dataframe
+    return user_df
